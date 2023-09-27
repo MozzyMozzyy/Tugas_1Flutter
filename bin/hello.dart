63049521f2ae55d60fce3a2871a37bd670a8c500
Muhammad Irfan <120771083+MozzyMozzyy@users.noreply.github.com>
2023-09-27 17:19:59 +0800

    tugas 1

---
+++ bin/hello.dart
@@ -0,0 +1,16 @@
+void main(){
+  String nama = "Irfan";
+  int umur = 22;
+  bool menikah = false;
+  double tinggi = 168.54;
+  var alamat = "Tanjung";
+
+  nama += " ";
+
+  print("Nama saya $nama");
+  print("Umur saya $umur tahun");
+  print("Tinggi saya $tinggi cm");
+  print("menikah $menikah");
+  print("alamat saya $alamat");
+
+}
